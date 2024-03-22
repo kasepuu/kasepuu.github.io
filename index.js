e442ba3985c20dc94c34ceb57284fd7e5f777c81
kasepuu <joel.meeks@outlook.com>
2024-03-22 12:09:21 +0200

    in-progress

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-console.log("javascript loaded")
-const gameDiv = document.getElementById("openGame")
+const gameDiv = document.getElementById("gameToggle")
+let gamePlaying = false
 
-gameDiv.addEventListener("click", () => {
-    console.log("hallohallo")
-})
+gameDiv.addEventListener("click", () => gamePlaying ? (closeGame(), gamePlaying = false) : (openGame(), gamePlaying = true));
+
+
+    function openGame(){
+        gameDiv.innerHTML = `
+        Close the game X
+        <div className="game">
+        <iframe
+          className="center-vertically"
+          src="http://backend.joelsoft.eu:1111"
+          title="make-your-game"
+        />
+      </div>`
+    }
+    function closeGame(){
+        gameDiv.innerHTML = `Play leprechaun the game!`
+    }
